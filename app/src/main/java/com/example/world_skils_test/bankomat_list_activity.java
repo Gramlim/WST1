6a6e0d18c561d45909ebc6104d5532c7cd02db1f
Stepan <Az7081488228>
2019-11-04 10:25:26 +0300

    N2V3

--- app/src/main/java/com/example/world_skils_test/bankomat_list_activity.java
+++ app/src/main/java/com/example/world_skils_test/bankomat_list_activity.java
@@ -2,26 +2,35 @@ package com.example.world_skils_test;
 
 import android.app.ListActivity;
 import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-//import com.yandex.mapkit.geometry.Point;
-//import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKitFactory;
-//import com.yandex.mapkit.map.CameraPosition;
-import com.yandex.mapkit.mapview.MapView;
 
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,23 +41,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class bankomat_list_activity extends ListActivity implements OnMapReadyCallback {
-    //private MapView mapview;
+
+    GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    private ListView listView;
+
     @Override
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_bank);
-        /*
-        MapKitFactory.setApiKey("f417b19f-469c-4852-bcd5-ce84f5a402bc");
-        MapKitFactory.initialize(this);
 
-        mapview = (MapView)findViewById(R.id.mapview);
-        mapview.getMap().move(
-                new CameraPosition(new Point(55.807068, 38.424886), 15.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
-                null);
 
-         */
-      MapFragment mapFragment = (MapFragment) getFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -76,32 +80,29 @@ public class bankomat_list_activity extends ListActivity implements OnMapReadyCa
                 tv.setText("Fail");
             }
         });
-    }
-    /*
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapview.onStop();
-        MapKitFactory.getInstance().onStop();
+
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapview.onStart();
-        MapKitFactory.getInstance().onStart();
-        }
 
-     */
-
-    void setDataToList(List<Home> building){
-        setListAdapter(new CustomAdapterAdres(this,building));
+    void setDataToList(List<Home> building) {
+        setListAdapter(new CustomAdapterAdres(this, building));
     }
 
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        this.mMap = map;
+        this.mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng).title("click"));
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(latLng);
+                mMap.animateCamera(cameraUpdate);
+            }
+        });
+
     }
+
+
+
 }
 
